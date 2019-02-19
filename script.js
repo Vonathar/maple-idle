b9064fb59f49ebc4b12f09e1677d26ff1dc0741c
Gianmarco <gianmarco.caputo.uk@gmail.com>
2019-02-19 23:52:05 +0000

    Basic working version

---
+++ script.js
@@ -0,0 +1,9 @@
+// The object used to keep all the main stats of the player
+let playerStats = {
+   currentLevel: 1,
+   currentExperience: 0,
+   requiredExperience: 10,
+   attackPower: 1,
+}
+
+// Importing from the DOM all the elements which get modified by JS
